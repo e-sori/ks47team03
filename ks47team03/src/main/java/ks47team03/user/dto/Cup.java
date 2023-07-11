e9bfc2e52989046f868d5a997e18149fae26566b
hyeonjeongeee <0hyeon22@naver.com>
2023-07-11 17:31:46 +0900

    [ks47team03] 컵 수정 화면 author : 박현정 detail : 컵 수정화면 hmtl 수정

--- ks47team03/src/main/java/ks47team03/user/dto/Cup.java
+++ ks47team03/src/main/java/ks47team03/user/dto/Cup.java
@@ -2,4 +2,57 @@ package ks47team03.user.dto;
 
 public class Cup {
 
+	private String cupQR;
+	private String staticCode;
+	private String setDateTime;
+	private String adminName;
+	private String upDateTime;
+	public String getCupQR() {
+		return cupQR;
+	}
+	public void setCupQR(String cupQR) {
+		this.cupQR = cupQR;
+	}
+	public String getStaticCode() {
+		return staticCode;
+	}
+	public void setStaticCode(String staticCode) {
+		this.staticCode = staticCode;
+	}
+	public String getSetDateTime() {
+		return setDateTime;
+	}
+	public void setSetDateTime(String setDateTime) {
+		this.setDateTime = setDateTime;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getUpDateTime() {
+		return upDateTime;
+	}
+	public void setUpDateTime(String upDateTime) {
+		this.upDateTime = upDateTime;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(", cupQR=");
+		builder.append(cupQR);
+		builder.append(", staticCode=");
+		builder.append(staticCode);
+		builder.append(", setDateTime=");
+		builder.append(setDateTime);
+		builder.append(", adminName=");
+		builder.append(adminName);
+		builder.append(", upDateTime=");
+		builder.append(upDateTime);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }
