565a04736f91dd87a5bdf18e2781d43a2e4a36c9
hyeonjeongeee <0hyeon22@naver.com>
2023-07-11 13:50:49 +0900

    [ks47team03] 컵 상태 관리 화면 구현, 관련 css수정 author : 박현정 detail : cupStateManage 컵 상태 조회 화면 구현 		 데이터 테이블 js 수정

--- ks47team03/src/main/java/ks47team03/admin/controller/AdminCupController.java
+++ ks47team03/src/main/java/ks47team03/admin/controller/AdminCupController.java
@@ -1,9 +1,13 @@
 package ks47team03.admin.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks47team03.admin.service.AdminCupService;
 
@@ -19,12 +23,27 @@ public class AdminCupController {
 	public AdminCupController(AdminCupService cupService) {
 		this.cupService = cupService;
 	}
-	// 컵 상태 관리
+	//컵 상태 관리
+	@SuppressWarnings("unchecked")
 	@GetMapping("/cupStateManage")
-	public String cupStateManage(Model model) {
-		model.addAttribute("title","구구컵 상태 관리");
+	public String cupStateManage(@RequestParam(value="currentPage", required = false ,defaultValue = "1")int currentPage,
+									Model model) {
+		//required= false 입력값 필수로 안받겠다. defaultValue = "1" 기본값 설정,문자열만 입력 가능 Modle=보내질 데이터
+		Map<String,Object> resultMap = cupService.getCupStateList(currentPage);
+		int lastPage = (int)resultMap.get("lastPage");
+		
+		List<Map<String,Object>> cupStateList = (List<Map<String,Object>>)resultMap.get("cupStateList");
+		int startPageNum = (int) resultMap.get("startPageNum");
+		int endPageNum = (int) resultMap.get("endPageNum");
+		model.addAttribute("title", "컵 상태 관리");
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("cupStateList", cupStateList);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
 		return "admin/cup/cupStateManage";
 	}
+	
 	// 컵 관련 기준 관리
 	@GetMapping("/cupManageStandard")
 	public String cupManageStandard(Model model) {
