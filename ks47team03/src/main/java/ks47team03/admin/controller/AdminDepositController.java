f15be0f79833b6bdcf49271ce28ba16204199ae5
csb7503 <chltnqhd010@naver.com>
2023-07-14 17:26:27 +0900

    [ks47team03] 보증금 관리자 페이지 조회 관련 DB연결 author : 최수봉 detail : 보증금 관리자 조회 관련 DB연결, 보증금 유저 페이지 결제버튼 추가(미완성)

--- ks47team03/src/main/java/ks47team03/admin/controller/AdminDepositController.java
+++ ks47team03/src/main/java/ks47team03/admin/controller/AdminDepositController.java
@@ -1,13 +1,18 @@
 package ks47team03.admin.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks47team03.admin.service.AdminDepositService;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @Controller
 @RequestMapping("/admin/deposit")
 public class AdminDepositController {
@@ -19,32 +24,116 @@ public class AdminDepositController {
 	public AdminDepositController(AdminDepositService depositService) {
 		this.depositService = depositService;
 	}
+
+	@GetMapping("/depositCheckSuccess")
+	public String depositCheckSuccess(Model model) {
+		model.addAttribute("title","결제 성공");
+		return "admin/deposit/depositCheckSuccess";
+	}
+	@GetMapping("/depositCheckfail")
+	public String depositCheckfail(Model model) {
+		model.addAttribute("title","결제 실패");
+		return "admin/deposit/depositCheckfail";
+	}
+	
+	
 	
 	//보증금 내역 관리
+	@SuppressWarnings("unchecked")
 	@GetMapping("/depositManage")
-	public String depositManage(Model model) {
-		model.addAttribute("title","보증금 내역 관리");
+	public String depositManage(@RequestParam(value="currentPage", required = false ,defaultValue = "1")int currentPage,
+			Model model) {
+		Map<String,Object> resultMap = depositService.getDepositManageList(currentPage);
+		int lastPage = (int)resultMap.get("lastPage");
+		List<Map<String,Object>> depositManageList = (List<Map<String,Object>>)resultMap.get("depositManageList");
+		log.info("depositManageList:{}",depositManageList);
+		model.addAttribute("title","회원 보증금 관리");
+		int startPageNum = (int) resultMap.get("startPageNum");
+		int endPageNum = (int) resultMap.get("endPageNum");
+		model.addAttribute("title","회원 보증금 관리");
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("depositManageList", depositManageList);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		
 		return "admin/deposit/depositManage";
 	}
+	
+	
+	
 	//보증금 결제 관리
+	@SuppressWarnings("unchecked")
 	@GetMapping("/depositPayManage")
-	public String depositPayManage(Model model) {
+	public String depositPayManage(@RequestParam(value="currentPage", required = false ,defaultValue = "1")int currentPage,
+			Model model) {
+		Map<String,Object> resultMap = depositService.getDepositPayList(currentPage);
+		int lastPage = (int)resultMap.get("lastPage");
+		List<Map<String,Object>> depositPayList = (List<Map<String,Object>>)resultMap.get("depositPayList");
+		log.info("depositPayList:{}",depositPayList);
 		model.addAttribute("title","보증금 결제 관리");
+		int startPageNum = (int) resultMap.get("startPageNum");
+		int endPageNum = (int) resultMap.get("endPageNum");
+	
+		model.addAttribute("title","보증금 결제 관리");
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("depositPayList", depositPayList);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		
 		return "admin/deposit/depositPayManage";
 	}
+	
 	//보증금 환급 관리
+	@SuppressWarnings("unchecked")
 	@GetMapping("/depositRefundManage")
-	public String depositRefundManage(Model model) {
+	public String depositRefundManage(@RequestParam(value="currentPage", required = false ,defaultValue = "1")int currentPage,
+			Model model) {
+		Map<String,Object> resultMap = depositService.getDepositRefundList(currentPage);
+		int lastPage = (int)resultMap.get("lastPage");
+		List<Map<String,Object>> depositRefundList = (List<Map<String,Object>>)resultMap.get("depositRefundList");
+		log.info("depositRefundList:{}",depositRefundList);
 		model.addAttribute("title","보증금 환급 관리");
+		int startPageNum = (int) resultMap.get("startPageNum");
+		int endPageNum = (int) resultMap.get("endPageNum");
+	
+		model.addAttribute("title","보증금 환급 관리");
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("depositRefundList", depositRefundList);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+	
 		return "admin/deposit/depositRefundManage";
 	}
+	
+	
 	// 보증금 기준 관리
+		@SuppressWarnings("unchecked")
 		@GetMapping("/depositStandard")
-		public String depositStandard(Model model) {
+		public String depositStandard(@RequestParam(value="currentPage", required = false ,defaultValue = "1")int currentPage,
+				Model model) {
+			Map<String,Object> resultMap = depositService.getDepositStandardList(currentPage);
+			int lastPage = (int)resultMap.get("lastPage");
+			
+			List<Map<String,Object>> depositStandardList = (List<Map<String,Object>>)resultMap.get("depositStandardList");
+			log.info("depositStandardList:{}",depositStandardList);
+			
+			int startPageNum = (int) resultMap.get("startPageNum");
+			int endPageNum = (int) resultMap.get("endPageNum");
+		
 			model.addAttribute("title","보증금 기준 관리");
+			model.addAttribute("currentPage", currentPage);
+			model.addAttribute("lastPage", lastPage);
+			model.addAttribute("depositStandardList", depositStandardList);
+			model.addAttribute("startPageNum", startPageNum);
+			model.addAttribute("endPageNum", endPageNum);
+			
 			return "admin/deposit/depositStandard";
 		}
 	
+		
 
 
 }
--- ks47team03/src/main/java/ks47team03/admin/mapper/AdminDepositMapper.java
+++ ks47team03/src/main/java/ks47team03/admin/mapper/AdminDepositMapper.java
@@ -1,5 +1,28 @@
 package ks47team03.admin.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AdminDepositMapper {
+
+	// 보증금 기준
+	public List<Map<String,Object>> getDepositStandardList(Map<String,Object>paramMap);
+	public int getDepositStandardListCount();
+
+	//보증금 결제
+	public List<Map<String,Object>> getDepositPayList(Map<String,Object>paramMap);
+	public int getDepositPayListCount();
+
+	//보증금 관리
+	public List<Map<String,Object>> getDepositManageList(Map<String,Object>paramMap);
+	public int getDepositManageListCount();
+	
+	//보증금 환급
+	public List<Map<String,Object>> getDepositRefundList(Map<String,Object>paramMap);
+	public int getDepositRefundListCount();
+
 
 }
