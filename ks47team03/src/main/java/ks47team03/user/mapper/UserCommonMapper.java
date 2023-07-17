cdbb821045e0e3dc4ad9b893dbc7037d679f670f
mildhyun <mildhyun1068@gmail.com>
2023-07-17 17:28:56 +0900

    [ks47team03] 로그인 생성 및 권한 부여 author : 현주열 detail : 로그인 생성 및 권한 부여

--- ks47team03/src/main/java/ks47team03/user/mapper/UserCommonMapper.java
+++ ks47team03/src/main/java/ks47team03/user/mapper/UserCommonMapper.java
@@ -1,6 +1,24 @@
 package ks47team03.user.mapper;
 
-public interface UserCommonMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import ks47team03.user.dto.User;
+
+@Mapper
+public interface UserCommonMapper {
 	
+	// 회원가입
+	public int joinUser(User user);
+
+	// 회원 상세 조회
+	public User getUserInfoById(String userId);
+	
+	// 회원등급 조회
+	public List<User> getUserLevelList();
+	
+	// 회원 중복 체크
+	public boolean idCheck(String userId);
+
 }
--- ks47team03/src/main/java/ks47team03/user/service/UserCommonService.java
+++ ks47team03/src/main/java/ks47team03/user/service/UserCommonService.java
@@ -1,8 +1,61 @@
 package ks47team03.user.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import ks47team03.user.dto.User;
+import ks47team03.user.mapper.UserCommonMapper;
 
 @Service
 public class UserCommonService {
-
+	
+	
+	private final UserCommonMapper userCommonMapper ;
+	
+	public UserCommonService(UserCommonMapper userCommonMapper) {
+		this.userCommonMapper = userCommonMapper;
+	}
+	
+		//회원 검증 여부
+		public Map<String, Object> isValidUser(String userId, String userPw) {
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			
+			boolean isValid = false;
+			
+			// 회원 검증 
+			User user = userCommonMapper.getUserInfoById(userId);
+			if(user != null) {
+				String checkPw = user.getUserPw();
+				if(checkPw.equals(userPw)) {
+					isValid = true;
+					resultMap.put("userInfo", user);
+				}
+			}
+			
+			resultMap.put("isValid", isValid);
+			
+			return resultMap;
+		}
+		
+		//회원 상세 조회
+		public User getUserInfoById(String userId) {
+			User userInfo = userCommonMapper.getUserInfoById(userId);
+			return userInfo;
+		}
+		
+		//회원 가입
+		public void joinUser(User user) {
+			userCommonMapper.joinUser(user);
+		}
+		
+		//회원 등급 조회
+		public List<User> getUserLevelList(){
+				
+			List<User> userLevelList = userCommonMapper.getUserLevelList();
+				
+			return userLevelList;
+		}
 }
