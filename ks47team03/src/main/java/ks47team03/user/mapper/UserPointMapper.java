cf81caf8c6f3d42cd09615712e9c47344a2fa50c
e-sori <leerri92@hanmail.net>
2023-07-21 17:32:47 +0900

    [ks47team03] 관리자 페이지 회원 포인트 관리 db 작성 author : 이소리 detail : 관리자 페이지 회원 포인트 관리 db 작성 및 연결

--- ks47team03/src/main/java/ks47team03/user/mapper/UserPointMapper.java
+++ ks47team03/src/main/java/ks47team03/user/mapper/UserPointMapper.java
@@ -1,8 +1,14 @@
 package ks47team03.user.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserPointMapper {
-
+	
+	public List<Map<String,Object>> getAllUserPoint(Map<String,Object> paraMap);
+	
+	public int getAllUserPointCount(String tableName);
 }
