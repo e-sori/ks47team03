b76d91c854568c2861bfbad764583c721b67d9e1
sseowon <tjdnjs0418@gmail.com>
2023-07-25 06:52:59 +0900

    [ks47team03] 커뮤니티 게시판 70퍼센트 구현 author : 서원 detail : 커뮤니티 게시판 70퍼센트 구현

--- ks47team03/src/main/java/ks47team03/user/service/UserBoardService.java
+++ ks47team03/src/main/java/ks47team03/user/service/UserBoardService.java
@@ -1,8 +1,31 @@
 package ks47team03.user.service;
 
+import ks47team03.user.dto.Board;
+import ks47team03.user.mapper.UserBoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserBoardService {
 
+    @Autowired
+    private UserBoardMapper userBoardMapper;
+
+    // 게시글 작성
+    public void boardWrite(Board board){
+
+        userBoardMapper.save(board);
+    }
+    // 게시글 리스트
+    public List<Board> communityBoardView(){
+
+        return userBoardMapper.findAll();
+    }
+    //게시글 상세 조회
+    public Board communityBoardDetail(String boardCode){
+
+        return userBoardMapper.findById(boardCode).get();
+    }
 }
