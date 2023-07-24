1ef362fdaf562992ca26a7ee2d220fa4e33edf53
e-sori <leerri92@hanmail.net>
2023-07-24 17:30:15 +0900

    [ks47team03] 관리자 페이지 포인트 관련 기준 페이지 동적으로 테이블 만들기 author : 이소리 detail : 관리자 페이지 포인트 관련 기준 페이지 동적으로 테이블 만들기

--- ks47team03/src/main/java/ks47team03/admin/controller/AdminPointController.java
+++ ks47team03/src/main/java/ks47team03/admin/controller/AdminPointController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -61,96 +62,71 @@ public class AdminPointController {
 		model.addAttribute("title","포인트 관련 기준 수정");
 		return "admin/point/modifyPointMaxCountStandard";
 	}		
+	
+	
+	// 포인트 관련 기준 관리 화면	(ajax로 데이터 받기)
+	@PostMapping("/pointStandardManage")
+	@SuppressWarnings("unchecked")
+	public String pointStandardMange(@RequestParam(value="currentPage", required = false, defaultValue = "1")int currentPage,
+										@RequestParam(value="tableId", required = false, defaultValue = "1")String tableId,
+										Model model,
+										HttpSession session) {
+		
+		Map<String,Object> pointStandardResultMap = adminPointService.getPointStandard(currentPage,tableId);
+		
+		
+		int startPageNum = (int)pointStandardResultMap.get("startPageNum");
+		int endPageNum = (int)pointStandardResultMap.get("endPageNum");
+		int lastPageNum = (int)pointStandardResultMap.get("lastPageNum");		
+		List<Map<String,Object>> pointStandardList = (List<Map<String,Object>>)pointStandardResultMap.get("pointStandardList");
+		String sessionId = (String) session.getAttribute("SID");
+		
+		
+		
+		model.addAttribute("title","포인트 관련 기준 관리");
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		model.addAttribute("lastPageNum", lastPageNum);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("pointStandardList", pointStandardList);	
+		model.addAttribute("SID", sessionId);
+		
+		if(tableId.equals("pills-max")) {
+			int useMaximumCount = (int) pointStandardList.get(0).get("useMaximumCount");
+			model.addAttribute("useMaximumCount", useMaximumCount);
+		}else if(tableId.equals("pills-use")) {
+			List<Map<String,Object>> codeUseList = (List<Map<String,Object>>) pointStandardResultMap.get("codeUseList");
+			model.addAttribute("codeUseList", codeUseList);
+		} // 나중에 모든 테이블 사용 유무 추가해주고 else if 없애기
+		
+		return "admin/point/pointStandardManage";
+	}
 
 	// 포인트 관련 기준 관리 화면	
 	@GetMapping("/pointStandardManage")
 	@SuppressWarnings("unchecked")
-	public String pointStandardManage(@RequestParam(value="currentPageMax", required = false, defaultValue = "1")int currentPageMax,
-										@RequestParam(value="currentPageExpire", required = false, defaultValue = "1")int currentPageExpire,
-										@RequestParam(value="currentPageType", required = false, defaultValue = "1")int currentPageType,
-										@RequestParam(value="currentPageSave", required = false, defaultValue = "1")int currentPageSave,
-										@RequestParam(value="currentPageRefund", required = false, defaultValue = "1")int currentPageRefund,
+	public String pointStandardManage(@RequestParam(value="currentPage", required = false, defaultValue = "1")int currentPage,
+										@RequestParam(value="tableId", required = false, defaultValue = "pills-max")String tableId,
 													Model model,
 													HttpSession session) {
 		
-		/* 5-2 포인트 환급 기준 조회 */
-		Map<String,Object> pointRefundStandardResultMap = adminPointService.getPointRefundStandard(currentPageRefund);
+		/* 포인트 기준 조회 */
+		Map<String,Object> pointStandardResultMap = adminPointService.getPointStandard(currentPage,tableId);
 		
-		int startPageNumRefund = (int)pointRefundStandardResultMap.get("startPageNumRefund");
-		int endPageNumRefund = (int)pointRefundStandardResultMap.get("endPageNumRefund");
-		int lastPageNumRefund = (int)pointRefundStandardResultMap.get("lastPageNumRefund");		
-		List<Map<String,Object>> pointRefundStandardList = (List<Map<String,Object>>)pointRefundStandardResultMap.get("pointRefundStandardList");
-		
-		model.addAttribute("startPageNumRefund", startPageNumRefund);
-		model.addAttribute("endPageNumRefund", endPageNumRefund);
-		model.addAttribute("lastPageNumRefund", lastPageNumRefund);
-		model.addAttribute("currentPageRefund", currentPageRefund);
-		model.addAttribute("pointRefundStandardList", pointRefundStandardList);
-		
-		/* 4-2 포인트 적립 기준 조회 */
-		Map<String,Object> pointSaveStandardResultMap = adminPointService.getPointSaveStandard(currentPageSave);
-		
-		int startPageNumSave = (int)pointSaveStandardResultMap.get("startPageNumSave");
-		int endPageNumSave = (int)pointSaveStandardResultMap.get("endPageNumSave");
-		int lastPageNumSave = (int)pointSaveStandardResultMap.get("lastPageNumSave");		
-		List<Map<String,Object>> pointSaveStandardList = (List<Map<String,Object>>)pointSaveStandardResultMap.get("pointSaveStandardList");
-		
-		model.addAttribute("startPageNumSave", startPageNumSave);
-		model.addAttribute("endPageNumSave", endPageNumSave);
-		model.addAttribute("lastPageNumSave", lastPageNumSave);
-		model.addAttribute("currentPageSave", currentPageSave);
-		model.addAttribute("pointSaveStandardList", pointSaveStandardList);
-		
-		/* 3-2 포인트 타입 기준 조회 */
-		Map<String,Object> pointTypeStandardResultMap = adminPointService.getPointTypeStandard(currentPageType);
-		
-		int startPageNumType = (int)pointTypeStandardResultMap.get("startPageNum");
-		int endPageNumType = (int)pointTypeStandardResultMap.get("endPageNum");
-		int lastPageNumType = (int)pointTypeStandardResultMap.get("lastPageNum");		
-		List<Map<String,Object>> pointTypeStandardList = (List<Map<String,Object>>)pointTypeStandardResultMap.get("pointTypeStandardList");
-		
-		model.addAttribute("startPageNumType", startPageNumType);
-		model.addAttribute("endPageNumType", endPageNumType);
-		model.addAttribute("lastPageNumType", lastPageNumType);
-		model.addAttribute("currentPageType", currentPageType);
-		model.addAttribute("pointTypeStandardList", pointTypeStandardList);
-		
-		/* 2-2 포인트 만료 기간 기준 조회 */
-		Map<String,Object> pointExpireStandardResultMap = adminPointService.getPointExpireStandard(currentPageExpire);
-		
-		int startPageNumExpire = (int)pointExpireStandardResultMap.get("startPageNumExpire");
-		int endPageNumExpire = (int)pointExpireStandardResultMap.get("endPageNumExpire");
-		int lastPageNumExpire = (int)pointExpireStandardResultMap.get("lastPageNumExpire");		
-		List<Map<String,Object>> pointExpireStandardList = (List<Map<String,Object>>)pointExpireStandardResultMap.get("pointExpireStandardList");
-		int pointExpire =  Integer.parseInt(String.valueOf(pointExpireStandardList.get(0).get("pointExpire")));
-		
-		model.addAttribute("startPageNumExpire", startPageNumExpire);
-		model.addAttribute("endPageNumExpire", endPageNumExpire);
-		model.addAttribute("lastPageNumExpire", lastPageNumExpire);
-		model.addAttribute("currentPageExpire", currentPageExpire);
-		model.addAttribute("pointExpireStandardList", pointExpireStandardList);
-		model.addAttribute("pointExpire", pointExpire);
-
-		
-		/* 1-2 하루 최대 적립 포인트 횟수 기준 조회 */
-		Map<String,Object> pointMaxCountStandardResultMap = adminPointService.getPointMaxCountStandard(currentPageMax);
-		
-		
-		int startPageNumMax = (int)pointMaxCountStandardResultMap.get("startPageNumMax");
-		int endPageNumMax = (int)pointMaxCountStandardResultMap.get("endPageNumMax");
-		int lastPageNumMax = (int)pointMaxCountStandardResultMap.get("lastPageNumMax");		
-		List<Map<String,Object>> pointMaxCountStandardList = (List<Map<String,Object>>)pointMaxCountStandardResultMap.get("pointMaxCountStandardList");
-		int useMaximumCount = (int) pointMaxCountStandardList.get(0).get("useMaximumCount");
+		int startPageNum = (int)pointStandardResultMap.get("startPageNum");
+		int endPageNum = (int)pointStandardResultMap.get("endPageNum");
+		int lastPageNum = (int)pointStandardResultMap.get("lastPageNum");		
+		List<Map<String,Object>> pointStandardList = (List<Map<String,Object>>)pointStandardResultMap.get("pointStandardList");
+		int useMaximumCount = (int) pointStandardList.get(0).get("useMaximumCount");
 		String sessionId = (String) session.getAttribute("SID");
-		List<Map<String,Object>> codeUseList = (List<Map<String,Object>>) pointMaxCountStandardResultMap.get("codeUseList");
-		
+		List<Map<String,Object>> codeUseList = (List<Map<String,Object>>) pointStandardResultMap.get("codeUseList");		
 		
 		model.addAttribute("title","포인트 관련 기준 관리");
-		model.addAttribute("startPageNumMax", startPageNumMax);
-		model.addAttribute("endPageNumMax", endPageNumMax);
-		model.addAttribute("lastPageNumMax", lastPageNumMax);
-		model.addAttribute("currentPageMax", currentPageMax);
-		model.addAttribute("pointMaxCountStandardList", pointMaxCountStandardList);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		model.addAttribute("lastPageNum", lastPageNum);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("pointStandardList", pointStandardList);
 		model.addAttribute("useMaximumCount", useMaximumCount);
 		model.addAttribute("codeUseList", codeUseList);
 		model.addAttribute("SID", sessionId);
---
+++ ks47team03/src/main/java/ks47team03/admin/mapper/AdminPointMapper2.java
@@ -0,0 +1,32 @@
+package ks47team03.admin.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+public interface AdminPointMapper2 {
+	
+	/* 5-2 포인트 환급 기준 조회 */
+	public List<Map<String,Object>> getPointRefundStandard(Map<String,Object> paramMap);
+	
+	/* 4-2 포인트 적립 기준 조회 */
+	public List<Map<String,Object>> getPointSaveStandard(Map<String,Object> paramMap);
+	
+	/* 3-2 포인트 타입 기준 조회 */
+	public List<Map<String,Object>> getPointTypeStandard(Map<String,Object> paramMap);
+	
+	/* 2-2 포인트 만료 기간 기준 조회 */
+	public List<Map<String,Object>> getPointExpireStandard(Map<String,Object> paramMap);
+	
+	/* 1-2 하루 최대 적립 포인트 횟수 기준 조회 */
+	public List<Map<String,Object>> getPointMaxCountStandard(Map<String,Object> paramMap);
+	
+	/* 데이터 개수 조회 */
+	public int getPointStandardCount(String tableName);
+	
+	/* 컬럼 내 중복 제거된 데이터 조회*/
+	public List<Map<String,Object>> getDistinctData(String tableName, String columnName);
+	
+}
