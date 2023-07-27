e2805c441e049d754a1d972d47ccdf71d4d5d656
hyeonjeongeee <0hyeon22@naver.com>
2023-07-27 09:53:27 +0900

    [ks47team03] 무인기기 위치조회 지도api연결 author : 박현정 detail : 무인기기 위치조회 지도 api연결, admin aside 이름 수정 		 kiosk,partner DTo 생성

--- ks47team03/src/main/java/ks47team03/user/controller/UserKioskController.java
+++ ks47team03/src/main/java/ks47team03/user/controller/UserKioskController.java
@@ -1,14 +1,23 @@
 package ks47team03.user.controller;
 
+
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import ks47team03.user.dto.Kiosk;
+import ks47team03.user.dto.Partner;
 import ks47team03.user.service.UserKioskService;
+import lombok.extern.slf4j.Slf4j;
 
 
-
+@Slf4j
 @Controller
 @RequestMapping("/kiosk")
 public class UserKioskController {
@@ -20,11 +29,25 @@ public class UserKioskController {
 	public UserKioskController(UserKioskService kioskService) {
 		this.kioskService = kioskService;
 	}
-	
+	//무인기기 위치 조회
+	@PostMapping("/kioskPlace")
+	@ResponseBody
+	public List<Partner> installedLocation() {
+		
+		List<Partner> kioskLocation = kioskService.getKioskLocationList();;
+		
+		return kioskLocation;
+	}
+	//무인기기 위치 조회
 	@GetMapping("/kioskPlace")
 	public String kioskPlace(Model model) {
-		
+
+		List<Partner> kioskLocation = kioskService.getKioskLocationList();
+		List<Kiosk> installedKiosk = kioskService.getinstalledKioskList();
 		model.addAttribute("title","무인기기 위치 조회");
+		
+		model.addAttribute("kioskLocation",kioskLocation);
+		model.addAttribute("installedKiosk",installedKiosk);
 		
 		return "user/kiosk/kioskPlace";
 	}
--- ks47team03/src/main/java/ks47team03/user/mapper/UserKioskMapper.java
+++ ks47team03/src/main/java/ks47team03/user/mapper/UserKioskMapper.java
@@ -1,5 +1,18 @@
 package ks47team03.user.mapper;
 
-public interface UserKioskMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import ks47team03.user.dto.Kiosk;
+import ks47team03.user.dto.Partner;
+
+@Mapper
+public interface UserKioskMapper {
+	//지도에 뿌릴 위도 경도 가져오기
+	public List<Partner> getKioskLocationList();
+	
+	//설치된 키오스크 리스트 가져오기
+	public List<Kiosk> getinstalledKioskList();
+	
 }
