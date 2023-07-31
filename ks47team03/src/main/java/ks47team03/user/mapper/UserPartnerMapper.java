da43f7208a78b19f782ef9217081e095817ceaf3
hyeonjeongeee <0hyeon22@naver.com>
2023-07-31 11:21:02 +0900

    [ks47team03] 관리자페이지 컵관련 수정,추가컵 배송 수정중 author : 박현정 detail : admin cup수정, 추가컵 배송 수정중

--- ks47team03/src/main/java/ks47team03/user/mapper/UserPartnerMapper.java
+++ ks47team03/src/main/java/ks47team03/user/mapper/UserPartnerMapper.java
@@ -1,5 +1,17 @@
 package ks47team03.user.mapper;
 
-public interface UserPartnerMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import ks47team03.user.dto.Kiosk;
+import ks47team03.user.dto.Partner;
+
+@Mapper
+public interface UserPartnerMapper {
+	public List<Kiosk>getInstalledKioskById(String loginId);
+	public void addBusinessCup(Partner partner);
+	//레벨과 아이디로 제휴업체 정보 조회
+	public List<Kiosk> getPartnerInfoByLevel(Map<String,Object> paramMap);
 }
