b31e2574b371f68f6dc2c7a3b4ecc80fa821695e
이소리 <leerri92@hanmail.net>
2023-08-15 19:10:13 +0900

    [ks47team03] 마이페이지 수정, 바코드 페이지 추가 author : 이소리 detail : 마이페이지 포인트 수정, css 수정 / 바코드 페이지 추가

--- ks47team03/src/main/java/ks47team03/user/controller/UserCupController.java
+++ ks47team03/src/main/java/ks47team03/user/controller/UserCupController.java
@@ -1,5 +1,8 @@
 package ks47team03.user.controller;
 
+import jakarta.servlet.http.HttpSession;
+import ks47team03.user.dto.Point;
+import ks47team03.user.service.UserPointService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +17,33 @@ public class UserCupController {
 	
 	// 의존성 주입
 	private final UserCupService cupService;
-	
-	
-	public UserCupController(UserCupService cupService) {
+	private final UserPointService userPointService;
+
+
+	public UserCupController(UserCupService cupService, UserPointService userPointService) {
 		this.cupService = cupService;
+		this.userPointService = userPointService;
+	}
+
+	//나의 바코드
+	@GetMapping("/myBarcode")
+	public String myBarcode(Model model) {
+		model.addAttribute("title","나의 바코드");
+
+		return "user/mypage/myBarcode";
 	}
 
 	// 나의 정보
 	@GetMapping("/myInfo")
-	public String myInfo(Model model) {			
-		model.addAttribute("title","나의 정보");			
+	public String myInfo(Model model, HttpSession session) {
+		String userId = (String) session.getAttribute("SID");
+		Point userPoint = userPointService.getUserPoint(userId);
+
+		int currentPoint = userPoint.getCurrentHoldingPoint();
+
+		model.addAttribute("title","나의 정보");
+		model.addAttribute("currentPoint",currentPoint);
+
 		return "user/mypage/myInfo";
 	}
 	
